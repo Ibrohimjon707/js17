9c73485c364114cf73ebc31c78a87cf8e8a1ac13
Ibrohimjon707 <ibrohimjonathamjonov38@gmail.com>
2025-02-19 17:55:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+let son = [1, 2, 3, 4, 5, 6];
+console.log(son.reverse());
+
+let yiginndi = 0
+let number = [1 + 2 + 3 + 4 + 5]
+console.log(number);
+
+let son1 = [2 * 1]
+console.log(`2 * 1 = ${son1}`);
+let a = 2 * 2
+console.log(`2 * 2 = ${a}`);
+let b = 2 * 3
+console.log(`2 * 3 = ${b}`);
+let d = 2 * 4
+console.log(`2 * 4 = ${d}`);
+let e = 2 * 5
+console.log(`2 * 5 = ${e}`);
+let h = 2 * 6
+console.log(`2 * 6 = ${h}`);
+let i = 2 * 7
+console.log(`2 * 7 = ${i}`);
+let k = 2 * 8
+console.log(`2 * 8 = ${k}`);
+let l = 2 * 9
+console.log(`2 * 9 = ${l}`);
+let m = 2 * 10
+console.log(`2 * 10 = ${m}`);
+
